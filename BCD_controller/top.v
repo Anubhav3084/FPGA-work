87dbd41b638e069e29ad18da6bda7bd5642b5278
Anubhav3084 <58735455+Anubhav3084@users.noreply.github.com>
2021-05-24 18:33:16 +0530

    Update top.v

--- BCD_controller/top.v
+++ BCD_controller/top.v
@@ -5,17 +5,15 @@
 // single module. 
 //////////////////////////////////////////////////////////////////////////////////
 
-module top(clk,count,load,reset,data_in,anode,out);
+module top(clk,ones,tens,hundreds,thousands,anode,out);
 	
-	input [3:0]data_in;
-	input clk,reset,load,count;
+	input [3:0]ones,tens,hundreds,thousands;
+	input clk;
 	
 	output [3:0]anode;
 	output [7:0]out;
 	
 	wire [1:0]sel;
-	wire [3:0]data,ones,tens,hundreds,thousands;
-	wire next1,next2,next3,next4;
 	
 	digit_select DS(
 	.clk(clk),
@@ -39,51 +37,6 @@ module top(clk,count,load,reset,data_in,anode,out);
 	BCD_convertor BCD( 
 	.data(data),
 	.out(out)
-	);
-	
-	clock_divider_freq1s clkfreq1(
-	.clk(clk),
-	.new_clk(new_clk)
-	);
-	
-	counter one(
-	.clk(new_clk),
-	.reset(reset),
-	.count(count),
-	.load(load),
-	.data_in(data_in),
-	.out(thousands),
-	.next(next1)
-	);
-	
-	counter ten(
-	.clk(next1),
-	.reset(reset),
-	.count(count),
-	.load(load),
-	.data_in(data_in),
-	.out(hundreds),
-	.next(next2)
-	);
-	
-	counter hundred(
-	.clk(next2),
-	.reset(reset),
-	.count(count),
-	.load(load),
-	.data_in(data_in),
-	.out(tens),
-	.next(next3)
-	);
-	
-	counter thousand(
-	.clk(next3),
-	.reset(reset),
-	.count(count),
-	.load(load),
-	.data_in(data_in),
-	.out(ones),
-	.next(next4)
 	);
 
 endmodule
